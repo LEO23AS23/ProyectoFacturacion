84115f67ac81bc9fe63d379b61081431bdfb0935
LEO23AS23 <pcastrol@uteq.edu.ec>
2026-03-05 13:03:21 -0500

    fix: agregar CORS al payment-service

--- payment-service/src/main/java/com/payment_service/payment_service/PaymentController.java
+++ payment-service/src/main/java/com/payment_service/payment_service/PaymentController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/payment")
 public class PaymentController {
 
