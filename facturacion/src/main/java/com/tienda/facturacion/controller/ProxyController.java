8b712194124054dea131caa684043a5111adff83
LEO23AS23 <pcastrol@uteq.edu.ec>
2026-03-09 14:58:48 -0500

    fix: implementacion clientesfinal

---
+++ facturacion/src/main/java/com/tienda/facturacion/controller/ProxyController.java
@@ -0,0 +1,21 @@
+package com.tienda.facturacion.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/proxy")
+@CrossOrigin(origins = "*")
+public class ProxyController {
+
+    @Value("${microservicio.clientes.url}")
+    private String clientesUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/clientes")
+    public Object obtenerClientes() {
+        return restTemplate.getForObject(clientesUrl + "/api/clientes", Object.class);
+    }
+}
